a8031c884c6891bda3abf2688918a10745ddae96
dawid <dawidholka97@gmail.com>
2021-12-23 14:27:05 +0100

    fix: profile page style

--- resources/js/Pages/Profile/Show.vue
+++ resources/js/Pages/Profile/Show.vue
@@ -1,35 +1,37 @@
 <template>
     <app-layout>
         <div>
-            <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
-                <div v-if="$page.props.jetstream.canUpdateProfileInformation">
-                    <update-profile-information-form :user="$page.props.user" class="mb-5" />
+            <div class="grid">
+                <div v-if="$page.props.jetstream.canUpdateProfileInformation" class="col-12">
+                    <update-profile-information-form :user="$page.props.user"/>
                 </div>
 
-                <div v-if="$page.props.jetstream.canUpdatePassword">
-                    <update-password-form class="mb-5"/>
+                <div v-if="$page.props.jetstream.canUpdatePassword" class="col-12">
+                    <update-password-form/>
                 </div>
 
-                <logout-other-browser-sessions-form :sessions="sessions" class="mt-10 sm:mt-0" />
+                <div class="col-12">
+                    <logout-other-browser-sessions-form :sessions="sessions" class="mt-10 sm:mt-0"/>
+                </div>
             </div>
         </div>
     </app-layout>
 </template>
 
 <script>
-    import AppLayout from '@/Layouts/AppLayout.vue'
-    import LogoutOtherBrowserSessionsForm from '@/Pages/Profile/Partials/LogoutOtherBrowserSessionsForm.vue'
-    import UpdatePasswordForm from '@/Pages/Profile/Partials/UpdatePasswordForm.vue'
-    import UpdateProfileInformationForm from '@/Pages/Profile/Partials/UpdateProfileInformationForm.vue'
+import AppLayout from '@/Layouts/AppLayout.vue'
+import LogoutOtherBrowserSessionsForm from '@/Pages/Profile/Partials/LogoutOtherBrowserSessionsForm.vue'
+import UpdatePasswordForm from '@/Pages/Profile/Partials/UpdatePasswordForm.vue'
+import UpdateProfileInformationForm from '@/Pages/Profile/Partials/UpdateProfileInformationForm.vue'
 
-    export default {
-        props: ['sessions'],
+export default {
+    props: ['sessions'],
 
-        components: {
-            AppLayout,
-            LogoutOtherBrowserSessionsForm,
-            UpdatePasswordForm,
-            UpdateProfileInformationForm,
-        },
-    }
+    components: {
+        AppLayout,
+        LogoutOtherBrowserSessionsForm,
+        UpdatePasswordForm,
+        UpdateProfileInformationForm,
+    },
+}
 </script>
